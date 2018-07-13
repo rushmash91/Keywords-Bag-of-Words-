50114e4802bf6dc0be253c93efa2e6a1c558cd54
arush sharma <arushsharma91@gmail.com>
2018-07-13 16:16:11 +0530

    tokanize and remove stopwords

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import PyPDF2
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+import string
 
 pdffile = open('JavaBasics-notes.pdf', 'rb')
 # rb because pdfs are binary files
@@ -14,4 +18,44 @@ for pages in range(num_pages - 2):
     page = reader.getPage(pages)
     add = page.extractText()
     all_string += add
-    print(add)
+
+all_string = all_string.lower()
+
+words = word_tokenize(all_string)
+count = 0
+
+stop_words = set(stopwords.words("english"))
+
+
+filtered = [word for word in words if not word in stop_words]
+filtered = [word for word in filtered if not word in set(string.punctuation)]
+
+print(filtered)
+
+freq = nltk.FreqDist(filtered)
+
+print(freq.most_common(1000))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
